0da8aea8a1566a8e5080cd5d561ebafc1b48955d
patrick <786156072@qq.com>
2022-10-25 16:28:05 +0800

    fix: level

--- src/logger.ts
+++ src/logger.ts
@@ -70,7 +70,5 @@ export class Logger {
 
 export default new Logger({
   out: window.console,
-  level: (process && process.env
-    && process.env.NODE_ENV === 'development')
-    ? Level.DEBUG : Level.ERROR
+  level: Level.ERROR
 })
